66d5bca125392455fb3147d047e172fca065bd56
r-ngtm <r_ngtm@yahoo.co.jp>
2018-12-05 23:49:35 +0900

    add : raymarching sample

---
+++ Assets/Scripts/SetVelocity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SetVelocity : MonoBehaviour
+{
+    [SerializeField] float m_AngularSpeed = 1f; // 角速度の係数
+    [SerializeField] Vector3 m_AngularSpeeds = new Vector3(-0.3f, 0.4f, -0.3f);  // 角速度
+    [SerializeField] float m_PositionSpeed = 1f; // 移動速度の係数
+    [SerializeField] Vector3 m_PositionSpeeds = new Vector3(-7f, -7.5f, -0.2f); // 移動速度
+
+    void Start()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = m_AngularSpeed * m_AngularSpeeds;
+        rigidbody.velocity = m_PositionSpeed * m_PositionSpeeds;
+        Debug.Log(rigidbody.velocity);
+
+    }
+}
---
+++ Assets/ShaderGraphExample/18_Raymarching_ManySphere/Scripts/CameraShaderSync.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraShaderSync : MonoBehaviour
+{
+    [SerializeField] Material _material; // カメラと同期させる対象のマテリアル
+    int _cameraUp;
+    int _cameraDir;
+    int _cameraPos;
+
+    void Start()
+    {
+        _cameraUp = Shader.PropertyToID("_CameraUp");
+        _cameraDir = Shader.PropertyToID("_CameraDir");
+        _cameraPos = Shader.PropertyToID("_CameraPos");
+    }
+
+    void Update()
+    {
+        // 位置と向きをシェーダーに渡す
+        _material.SetVector(_cameraUp, transform.up);
+        _material.SetVector(_cameraDir, transform.forward);
+        _material.SetVector(_cameraPos, transform.position);
+    }
+}
